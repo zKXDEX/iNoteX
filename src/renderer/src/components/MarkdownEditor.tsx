978da94116b064dba1ad54777ad2c990575a9c76
zkxdex <deiividdlk@gmail.com>
2024-02-04 18:13:34 +0100

    fixed timezone

--- src/renderer/src/components/MarkdownEditor.tsx
+++ src/renderer/src/components/MarkdownEditor.tsx
@@ -2,10 +2,13 @@ import {
   MDXEditor,
   headingsPlugin,
   listsPlugin,
+  linkPlugin,
   markdownShortcutPlugin,
   quotePlugin
 } from '@mdxeditor/editor'
 import { useMarkdownEditor } from '@renderer/hooks/useMarkdownEditor'
+
+
 
 export const MarkdownEditor = () => {
   const { editorRef, selectedNote, handleAutoSaving, handleBlur } = useMarkdownEditor()
@@ -19,7 +22,7 @@ export const MarkdownEditor = () => {
       markdown={selectedNote.content}
       onChange={handleAutoSaving}
       onBlur={handleBlur}
-      plugins={[headingsPlugin(), listsPlugin(), quotePlugin(), markdownShortcutPlugin()]}
+      plugins={[headingsPlugin(), listsPlugin(), quotePlugin(), markdownShortcutPlugin(), linkPlugin()]}
       contentEditableClassName="outline-none min-h-screen max-w-none text-lg px-8 py-5 caret-yellow-500 prose prose-invert prose-p:my-3 prose-p:leading-relaxed prose-headings:my-4 prose-blockquote:my-4 prose-ul:my-2 prose-li:my-0 prose-code:px-1 prose-code:text-red-500 prose-code:before:content-[''] prose-code:after:content-['']"
     />
   )
--- src/renderer/src/utils/index.ts
+++ src/renderer/src/utils/index.ts
@@ -4,7 +4,6 @@ import { twMerge } from 'tailwind-merge'
 const dateFormatter = new Intl.DateTimeFormat(window.context.locale, {
   dateStyle: 'short',
   timeStyle: 'short',
-  timeZone: 'UTC'
 })
 
 export const formatDateFromMs = (ms: number) => dateFormatter.format(ms)
